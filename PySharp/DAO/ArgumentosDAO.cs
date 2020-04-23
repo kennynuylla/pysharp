10fbc10a7a3760404d276975a8989740e562d412
Cícero Lacerda <matheuslacerda.08@gmail.com>
2020-04-23 12:27:47 -0300

    Argumentos para o script python

---
+++ PySharp/DAO/ArgumentosDAO.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+
+namespace PySharp.DAO
+{
+    public class ArgumentosDAO
+    {
+        private List<string> _argumentos = new List<string>();
+
+        string this[int i]
+        {
+            get => _argumentos[i];
+            set => _argumentos[i] = value;
+        }
+        public int Count { get => _argumentos.Count; }
+
+        public void AdicionarArgumento(string argumento)
+        {
+            _argumentos.Add(argumento);
+        }
+
+        public override string ToString()
+        {
+            var retorno = string.Empty;
+            if(Count == 0) return retorno;
+
+            retorno = _argumentos[0];
+            for (int i = 1; i < Count; i++)
+            {
+                retorno = $"{retorno} {_argumentos[i]}";
+            }
+
+            return retorno;
+        } 
+    }
+}
